09535c969fd217a174ff0184514a19b1bb1787cf
David Tekan <david.tekan@sap.com>
2020-05-13 15:24:18 -0700

    print

--- modules/recipe/components/RecipeHeader.js
+++ modules/recipe/components/RecipeHeader.js
@@ -15,9 +15,6 @@ const RecipeHeader = ({ photo, title, rating, addToMenu }) => {
         </div>
         <div className="row options print-hidden">
           <div className="col-xs-12">
-            <button className="btn btn-primary" onClick={addToMenu}>
-              <span className="glyphicon glyphicon-calendar" aria-hidden="true"/>
-            </button>
             <button className="btn btn-primary" onClick={ window.print }>
               <span className="glyphicon glyphicon-print" aria-hidden="true"/>
             </button>
@@ -33,6 +30,13 @@ const RecipeHeader = ({ photo, title, rating, addToMenu }) => {
         <div className="col-xs-12">
           <h3>{ title }</h3>
           <Ratings stars={ rating }/>
+        </div>
+      </div>
+      <div className="row options print-hidden">
+        <div className="col-xs-12">
+          <button className="btn btn-primary" onClick={ window.print }>
+            <span className="glyphicon glyphicon-print" aria-hidden="true"/>
+          </button>
         </div>
       </div>
     </div>
